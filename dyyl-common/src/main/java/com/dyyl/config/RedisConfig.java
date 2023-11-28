9e802180f664c22bc7ac5dd4af8ba31c6e148eb0
cj <chengjian0907@163.com>
2023-11-28 18:31:44 +0800

    引入redis缓存

---
+++ dyyl-common/src/main/java/com/dyyl/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.dyyl.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author cj
+ * @Description
+ * @Date 2023/11/28
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+
+        Jackson2JsonRedisSerializer<Object> jsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jsonRedisSerializer.setObjectMapper(objectMapper);
+
+        redisTemplate.setValueSerializer(jsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+}
--- dyyl-system/src/test/java/com/dyyl/DyylSystemApplicationTests.java
+++ dyyl-system/src/test/java/com/dyyl/DyylSystemApplicationTests.java
@@ -1,11 +1,25 @@
 package com.dyyl;
 
+import com.dyyl.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class DyylSystemApplicationTests {
+
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Test
     void contextLoads() {
@@ -15,4 +29,22 @@ class DyylSystemApplicationTests {
     public void testUserPwd() {
     }
 
+    @Test
+    void redisSet() {
+//        redisTemplate.opsForValue().set("redisTest","hello redis");
+        List<String> list = new ArrayList<>();
+        list.add("zhangsan");
+        list.add("lisi");
+        list.add("wangwu");
+        list.add("wangwu");
+//        redisTemplate.opsForList().leftPush("listTest",list);
+        redisUtil.leftPush("listUtil",list);
+    }
+
+    @Test
+    void redisGet() {
+//        System.out.println(redisTemplate.opsForValue().get("redisTest"));
+//        System.out.println(redisTemplate.opsForList().range("listTest",0,-1));
+        System.out.println(redisUtil.range("listUtil", 0, -1));
+    }
 }
